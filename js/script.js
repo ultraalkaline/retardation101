ad328c0562979c1701bc11aba618d71e02a58f3a
Jovan Polus <jovanpolus97@gmail.com>
2017-01-31 11:38:30 +0100

    Added rocket555 soundfiles
    Also changed color scheme

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 $(window).load(function(){
-    var audiosPath = new Array('aah1.ogg', 'aah2.ogg', 'aah3.ogg', 'aah4.ogg', 'aah5.ogg', 'cuck1.ogg', 'cum1.ogg', 'cum2.ogg', 'cum3.ogg', 'cum4.ogg', 'cum5.ogg', 'cum6.ogg', 'cum7.ogg', 'gay1.ogg', 'gay2.ogg', 'idubbbztv.ogg', 'laff1.ogg', 'laff2.ogg', 'laff3.ogg', 'meme1.ogg', 'meme2.ogg', 'meme3.ogg', 'ohno1.ogg', 'ohno2.ogg', 'ohno3.ogg', 'ohno4.ogg', 'ohno5.ogg', 'retardation1.ogg', 'retardation2.ogg', 'retardation3.ogg', 'retardation4.ogg', 'retardation5.ogg', 'retardation6.ogg', 'retardation7.ogg', 'retardation8.ogg', 'retardation9.ogg', 'scat1.ogg', 'theory1.ogg', 'theory2.ogg', 'wheezing1.ogg', 'wheezing2.ogg', 'wheezing3.ogg', 'wheezing4.ogg', 'whitemaleprivilege1.ogg', 'who1.ogg');
+    var audiosPath = new Array('aah1.ogg', 'aah2.ogg', 'aah3.ogg', 'aah4.ogg',
+        'aah5.ogg', 'alla_ni_alla_ni.ogg', 'allahu1.ogg', 'allahu2.ogg',
+        'bitch.ogg', 'bög1.ogg', 'bög2.ogg', 'calle_är_bög.ogg', 'cough.ogg',
+        'cuck1.ogg', 'cum1.ogg', 'cum2.ogg', 'cum3.ogg', 'cum4.ogg', 'cum5.ogg',
+        'cum6.ogg', 'cum7.ogg', 'fuckyou.ogg', 'fyfan.ogg', 'gat2sonk.ogg',
+        'gay1.ogg', 'gay2.ogg', 'gay3.ogg', 'hölyshit.ogg', 'idubbbztv.ogg',
+        'ja(g).ogg', 'laff1.ogg', 'laff2.ogg', 'laff3.ogg', 'laff4.ogg',
+        'lol.ogg', 'meme1.ogg', 'meme2.ogg', 'meme3.ogg', 'noor_brinner.ogg',
+        'ohno1.ogg', 'ohno2.ogg', 'ohno3.ogg', 'ohno4.ogg', 'ohno5.ogg',
+        'ohno6.ogg', 'remove.ogg', 'retardation1.ogg', 'retardation2.ogg',
+        'retardation3.ogg', 'retardation4.ogg', 'retardation5.ogg',
+        'retardation6.ogg', 'retardation7.ogg', 'retardation8.ogg',
+        'retardation9.ogg', 'scat1.ogg', 'theory1.ogg', 'theory2.ogg',
+        'waduhek1.ogg', 'waduhek2.ogg', 'walla.ogg', 'what1.ogg',
+        'wheezing1.ogg', 'wheezing2.ogg', 'wheezing3.ogg', 'wheezing4.ogg',
+        'whitemaleprivilege1.ogg', 'yeah.ogg', 'är.ogg');
     var pathConst = "audio/";
     var audios = audiosPath;
 
